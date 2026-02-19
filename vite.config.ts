89cb713315e8148d87cf9eba9976e2dc8d05d3d8
Zham-Pierrot <dangp.zham@gmail.com>
2026-02-19 10:41:14 -0600

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/rentaway-home-show/", // 🔥 MUY IMPORTANTE
+  base: "/", 
 
   server: {
     host: "::",
